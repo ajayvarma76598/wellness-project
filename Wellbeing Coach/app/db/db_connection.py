c4e41f9ff5b3e235e32b4a0fe352b6a1fe81c5ee
Ajay-Penmetcha <pajayvarmap@gmail.com>
2026-04-04 10:53:36 +0530

    changes done

--- Wellbeing Coach/app/db/db_connection.py
+++ Wellbeing Coach/app/db/db_connection.py
@@ -8,11 +8,7 @@ load_dotenv()
 def get_connection():
     try:
         conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD")
+            os.getenv("POSTGRES_PRISMA_URL")
         )
         return conn
     except Exception as e:
